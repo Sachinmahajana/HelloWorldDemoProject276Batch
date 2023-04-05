fa3612c756e12c370879aa3b7fff849d91e26c51
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-05 23:30:06 +0530

    [Sachin] Adding Welcome message

---
+++ HelloWorldDemoProject276Batch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorldDemoProject276Batch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to HelloWorld Program!");
+        }
+    }
+}
