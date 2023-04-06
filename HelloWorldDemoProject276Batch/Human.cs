fd2f181ed7f2e3dfc11b4ec1ec57189fee6956b3
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-06 12:30:39 +0530

    [Sachin] Adding human program with instance members

---
+++ HelloWorldDemoProject276Batch/Human.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDemoProject276Batch
+{
+    public class Human
+    {
+        //instance variables
+        //name,height,weight,Age
+        //syntax:acessmodifier datatype variable
+        public string name;
+        public float height, weight;
+        public int Age;
+
+        // instance
+        public void Speak() //method defination/Signature
+        {
+            //method body or implementation 
+            Console.WriteLine("Human can speak");
+        }
+        //instance
+        public void HumanDetails(string humanName, float humanHeight, float humanWeight, int humanAge) //method defination/Signature
+        {
+            //method body or implementation
+            Console.WriteLine("Human Details:{0} {1} {2} {3}", humanName, humanHeight, humanWeight, humanAge);
+        }
+    }
+}
--- HelloWorldDemoProject276Batch/Program.cs
+++ HelloWorldDemoProject276Batch/Program.cs
@@ -8,6 +8,20 @@
             Console.WriteLine("Push the code on github");   
             Console.WriteLine("Modified in remote repository");
             Console.WriteLine("Modified in local repository");
+
+            //object or instance syntax
+            //Classname variable = new Classname
+            Human obj = new Human();
+            Console.WriteLine("Enter human name");
+            obj.name = Console.ReadLine();
+            Console.WriteLine("Please enter your height");
+            obj.height = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Please enter your weight");
+            obj.weight = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Please enter your age");
+            obj.Age = Convert.ToInt32(Console.ReadLine());
+            obj.Speak();
+            obj.HumanDetails(obj.name, obj.height, obj.weight, obj.Age);
         }
     }
 }
